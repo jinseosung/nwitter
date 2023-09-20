a5a212b5af4ba61681476250292729437b16cc4d
Jinseo Sung <jinseosung15@gmail.com>
2023-09-20 16:45:08 +0200

    log in setup

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,74 @@
-import React from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setpPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setpPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Mot de passe"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Créer un compte" : "Se connecter"}
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Se connecter" : "Créer un compte"}
+      </span>
+      <div>
+        <button>Continuer avec Google</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
