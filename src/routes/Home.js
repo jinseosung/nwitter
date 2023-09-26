bb1887a96357037f94ca0260d1407d35eb64a7ba
Jinseo Sung <jinseosung15@gmail.com>
2023-09-26 14:29:11 +0200

    database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="Dis-moi tout"
+          maxLength={120}
+        />
+        <input type="submit" value="Ntweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
