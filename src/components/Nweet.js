551bc7685a107e65ace3e7bd14046586bbd49054
Jinseo Sung <jinseosung15@gmail.com>
2023-09-28 11:36:10 +0200

    delete and update setup

---
+++ src/components/Nweet.js
@@ -0,0 +1,57 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Voulez-vous supprimer ce nweet ?");
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
+    toggleEditing(false);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              placeholder="Modifiez votre nweet"
+              value={newNweet}
+              onChange={onChange}
+              type="text"
+              required
+            />
+            <button onClick={toggleEditing}>Annuler</button>
+            <input type="submit" value="Modifier" />
+          </form>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Supprimer</button>
+              <button onClick={toggleEditing}>Modifier</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
-  const [nweets, setNweets] = useState("");
+  const [nweets, setNweets] = useState([]);
 
   useEffect(() => {
     const getNweets = async () => {
@@ -58,9 +59,11 @@ const Home = ({ userObj }) => {
       <div>
         {nweets &&
           nweets.map((nweet) => (
-            <div key={nweet.id}>
-              <h4>{nweet.text}</h4>
-            </div>
+            <Nweet
+              key={nweet.id}
+              nweetObj={nweet}
+              isOwner={nweet.creatorId === userObj.uid ? true : false}
+            />
           ))}
       </div>
     </div>
